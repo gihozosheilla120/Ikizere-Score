40c183e388e8cc8b269092645d937951489ef467
gihozosheilla120 <gihozosheilla.120@gmail.com>
2026-06-25 15:10:40 +0200

    updated

--- src/server.js
+++ src/server.js
@@ -3,11 +3,15 @@ const connectDatabase = require('./config/database');
 const env = require('./config/env');
 const { seedRecordCategories } = require('./seeds/recordCategories.seed');
 
+const { startScoreWorker } = require('./queues/scoreWorker');
+
 async function startServer() {
   await connectDatabase();
   await seedRecordCategories();
 
   const app = createApp();
+
+  await startScoreWorker();
 
   app.listen(env.port, () => {
     console.log(`Server running on port ${env.port}`);
