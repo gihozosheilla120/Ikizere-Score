ed6e424c816adc4d1d5f44f24b0e32c57301903d
gihozosheilla120 <gihozosheilla.120@gmail.com>
2026-06-25 15:16:55 +0200

    updated

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,14 @@ const authRoutes = require('../modules/auth/auth.routes');
 const businessProfileRoutes = require('../modules/business-profile/business-profile.routes');
 const verificationRoutes = require('../modules/verification/verification.routes');
 const financialRecordsRoutes = require('../modules/financial-records/financial-records.routes');
+const scoreRoutes = require('../modules/score/score.routes');
+const loanMarketplaceRoutes = require('../modules/loan-marketplace/loan-marketplace.routes');
 
 module.exports = {
   auth: authRoutes,
   users: businessProfileRoutes,
   verification: verificationRoutes,
   records: financialRecordsRoutes,
+  score: scoreRoutes,
+  loans: loanMarketplaceRoutes,
 };
